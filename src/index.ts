49fe6bd61ddbe20eb52b89c3f824eb8b1920f90f
Marc O'Cleirigh <marcocleirigh@gmail.com>
2025-01-26 17:10:48 -0800

    Fixed exports to include hook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { source, getMetadata, getKey, getTarget } from './core/PorterSource';
 export { connect } from './core/PorterAgent';
+export { usePorter } from './react/usePorter';
 export * from './porter.model';
