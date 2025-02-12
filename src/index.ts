8dfed63198011c03edaa65a69b9b54189537eb2e
Marc O'Cleirigh <marcocleirigh@gmail.com>
2025-02-11 17:08:27 -0800

    Export Logger and logging types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { source, getMetadata, getKey, getTarget } from './core/PorterSource';
 export { connect } from './core/PorterAgent';
 export { usePorter } from './react/usePorter';
+export { Logger, LogLevel, LoggerOptions } from './porter.utils';
 export * from './porter.model';
