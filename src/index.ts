6f27591a08427b922c6d2f31ad81232cd036c871
Marc O'Cleirigh <marcocleirigh@gmail.com>
2025-03-13 19:42:31 -0700

    Changed export of PorterSource to an object with named properties for all functions. Made instances a Map of namespaces to PorterSources

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-export {
-  source,
-  getAgentById,
-  getAgentByLocation,
-  queryAgents,
-} from './core/PorterSource';
+export { source } from './core/PorterSource';
 export { connect } from './core/PorterAgent';
 export { usePorter } from './react/usePorter';
 export { Logger, LogLevel, LoggerOptions } from './porter.utils';
